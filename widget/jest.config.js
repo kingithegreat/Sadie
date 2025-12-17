605dbb0dd3a8782512d6c411a1a7127f5af3183a
kingithegreat <139932842+kingithegreat@users.noreply.github.com>
2025-12-17 17:58:22 +1300

    Use shared electron mock in ipc registration test

--- widget/jest.config.js
+++ widget/jest.config.js
@@ -4,7 +4,15 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/src/renderer/setupTests.ts'],
+  setupFilesAfterEnv: [
+    '<rootDir>/src/renderer/setupTests.ts',
+    '<rootDir>/src/main/__tests__/jest-setup-after-env.ts',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/main/__tests__/jest-setup.ts',
+    '<rootDir>/src/main/__tests__/jest-setup-after-env.ts',
+  ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
